453123863b61f45e5b7b7434c4d1f679f6ce0642
Taylor Huffman <taylor@crucialartists.com>
2022-02-07 16:25:40 -0600

    Added different solutions for code along

--- index.js
+++ index.js
@@ -10,3 +10,63 @@ const messageConfig = {
       "Loki"
   ]
 };
+
+
+// // Solution 1a
+// const printCard = function () {
+//   console.log(this.frontContent);
+//   console.log(this.insideContent);
+
+//   this.signatories.forEach(function (signatory) {
+//     const message = `${this.closing[signatory]}, ${signatory}`;
+//     console.log(message);
+//   }, this);
+// };
+
+// printCard.call(messageConfig);
+
+
+
+// // Solution 1b
+// const printCard = function () {
+//   console.log(this.frontContent);
+//   console.log(this.insideContent);
+//   const contextBoundForEachExpr = function (signatory) {
+//     const message = `${this.closing[signatory]}, ${signatory}`;
+//     console.log(message);
+//   }.bind(this);
+
+//   this.signatories.forEach(contextBoundForEachExpr);
+// };
+
+// printCard.call(messageConfig);
+
+
+// // Solution 2
+// const printCard = function () {
+//   console.log(this.frontContent);
+//   console.log(this.insideContent);
+
+//   const outerContext = this;
+
+//   this.signatories.forEach(function (signatory) {
+//     const message = `${outerContext.closing[signatory]}, ${signatory}`;
+//     console.log(message);
+//   });
+// };
+
+// printCard.call(messageConfig);
+
+
+// // Solution 3
+// const printCard = function () {
+//   console.log(this.frontContent);
+//   console.log(this.insideContent);
+//   // Wow! Elegant! And notice the arrow function's `this` is the same `this`
+//   // that printCard has; specifically, the `thisArg` that was passed to it
+//   this.signatories.forEach((signatory) =>
+//     console.log(`${this.closing[signatory]}, ${signatory}`)
+//   );
+// };
+
+// printCard.call(messageConfig);
